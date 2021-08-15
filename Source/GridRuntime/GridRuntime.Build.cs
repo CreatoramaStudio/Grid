8933120b2cb146ed498e97bf0e0c16e387f28a04
fabriciomsantos <fabriciomoreira19@gmail.com>
2021-08-15 17:49:54 -0300

    Updated Build.cs

--- Source/GridRuntime/GridRuntime.Build.cs
+++ Source/GridRuntime/GridRuntime.Build.cs
@@ -3,52 +3,57 @@ namespace UnrealBuildTool.Rules
 	public class GridRuntime : ModuleRules
 	{
 		public GridRuntime(ReadOnlyTargetRules Target) : base(Target)
-        {
+		{
 			PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 			bEnforceIWYU = true;
 			bUseUnity = false;
 			ShadowVariableWarningLevel = WarningLevel.Error;
 
 			PublicIncludePaths.AddRange(
-				new string[] {
+				new string[]
+				{
 					// ... add public include paths required here ...
 				}
-				);
+			);
 
-			PrivateIncludePaths.AddRange(
-				new string[] {
-					"GridRuntime/Private",
+			PrivateIncludePaths.AddRange
+			(
+				new string[]
+				{
 					// ... add other private include paths required here ...
 				}
-				);
+			);
 
-			PublicDependencyModuleNames.AddRange(
+			PublicDependencyModuleNames.AddRange
+			(
 				new string[]
 				{
 					"Core",
 					"CoreUObject",
-                    "Engine",
-                    "AIModule",
-                    "RenderCore",
+					"Engine",
+					"AIModule",
+					"RenderCore",
 					"NavigationSystem",
 					"GameplayTags"
 					// ... add other public dependencies that you statically link with here ...
 				}
-				);
+			);
 
-			PrivateDependencyModuleNames.AddRange(
+			PrivateDependencyModuleNames.AddRange
+			(
 				new string[]
 				{
 					// ... add private dependencies that you statically link with here ...
 				}
-				);
+			);
 
-			DynamicallyLoadedModuleNames.AddRange(
+			DynamicallyLoadedModuleNames.AddRange
+			(
 				new string[]
 				{
 					// ... add any modules that your module loads dynamically here ...
 				}
-				);
+			);
 		}
 	}
 }
